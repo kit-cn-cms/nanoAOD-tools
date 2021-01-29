19fec03b4d1a77eec380fb4d727287bfdba2df93
sebwieland <wielandseb@googlemail.com>
2021-01-29 16:25:27 +0100

    changes for JECs

--- crab/crab_script.py
+++ crab/crab_script.py
@@ -2,19 +2,28 @@
 import os
 import sys
 import PSet
+import glob
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
 from importlib import import_module
+from pprint import pprint
 
 # this takes care of converting the input files from CRAB
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles, runsAndLumis
 
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import createJMECorrector
+
+
 def writeKeepOutFile(vars):
   with open("keepOut.txt", "w") as file:
     file.write("drop *\n")
+    # file.write("keep *MET*"+"\n")
+    # file.write("keep *"+"\n")
     for v in vars:
-      file.write("keep " + v+"\n")
+        file.write("keep " + v+"\n")
 
+schemes = ["Total","Merged","All"]
+default_JEC_scheme = "Total"
 
 from optparse import OptionParser
 parser = OptionParser(usage="")
@@ -22,12 +31,37 @@ parser.add_option("-t", "--test", dest="test", type="string", default="THIS IS A
                   help="Testoption")
 parser.add_option("-I", "--import", dest="imports", type="string", default=[], action="append",
                   nargs=2, help="Import modules (python package, comma-separated list of ")
+parser.add_option("--noRedoJEC", dest="noRedoJEC", default=False, action="store_true",
+                  help="DO NOT reapply latest JECs and run systs")
+parser.add_option("--JEC", dest="JECscheme",
+                  type = "choice", choices = schemes, 
+                  default = default_JEC_scheme,
+                  help = "Which JEC scheme should be run? Valid choices are %s. Default: %s"\
+                         % (schemes, default_JEC_scheme)
+                )
+parser.add_option("-e", "--era", dest="era", type="string", default="2017",
+                  help="Specify era of the dataset. default: 2017")
+parser.add_option("--isData", dest="isData", default=False, action="store_true",
+                  help="Specify if dataset is real data.")
+
 
 (options, args) = parser.parse_args()
 
 options.imports = [('PhysicsTools.NanoAODTools.postprocessing.examples.variableCalculatorModule', 'variableCalculatorModuleConstr')]
 
-modules = []
+if not options.noRedoJEC:
+    # jetmetCorrector = createJMECorrector(isMC=True, dataYear=2017, jesUncert="Merged", applyHEMfix=False) 
+    jetmetCorrector = createJMECorrector(isMC=not options.isData, dataYear=options.era, jesUncert=options.JECscheme, applyHEMfix="2018" in options.era ) 
+    jecs = ["nom", "jerUp", "jerDown"] + ["jes" + s + "Up" for s in jetmetCorrector().jesUncertainties] + ["jes" + s + "Down" for s in jetmetCorrector().jesUncertainties]
+    print("doing following JEC uncs:")
+    print(jecs)
+
+    modules = [jetmetCorrector()]
+else:
+    modules = []
+    jecs = []
+
+# load modules 
 for mod, names in options.imports:
     import_module(mod)
     obj = sys.modules[mod]
@@ -37,33 +71,56 @@ for mod, names in options.imports:
         if name in mods:
             print("Loading %s from %s " % (name, mod))
             if "variableCalculatorModule" in name:
-              mod = getattr(obj,name)()
-              mod.setOptions(test=options.test)   # pass options to module here!
-              varsTosave = mod.var_calc.output_values.keys() + mod.var_calc.output_arrays.keys()
-              writeKeepOutFile(varsTosave)
-              modules.append(mod)
-              continue
-            if type(getattr(obj, name)) == list:
-              if "variableCalculatorModule" in name:
-                for mod in getattr(obj, name):
-                    modules.append(mod())
-            else:
-                modules.append(getattr(obj, name)())
+                mod = getattr(obj,name)()
+                mod.setOptions(test=options.test, jecs = jecs)   # pass options to module here!
+                varsTosave = []
+                for sys in jecs:
+                    varsTosave += mod.var_calc.output_ValueswJEC[sys].keys()
+                    try:
+                        varsTosave += mod.var_calc.output_ArrayswJEC[sys].keys() 
+                    except:
+                        print("Didn't find key '{key}' in output_ArrayswJEC for module {name}".format(key=sys, name = name))
+                # varsTosave = mod.var_calc.output_values.keys() + mod.var_calc.output_arrays.keys()
+                writeKeepOutFile(varsTosave)
+                modules.append(mod)
+                continue
+            # if type(getattr(obj, name)) == list:
+            #     for mod in getattr(obj, name):
+            #         modules.append(mod())
+            # else:
+            #     modules.append(getattr(obj, name)())
 
 print("running following modules")
 print(modules)
 
+# p = PostProcessor(outputDir = ".",
+#                   inputFiles = inputFiles(),
+#                 #   cut = "",
+#                   modules=modules,
+#                   provenance=True,
+#                   fwkJobReport=True,
+#                   jsonInput=runsAndLumis(),
+#                   # maxEntries = 2000,
+#                   friend = False,
+#                   haddFileName = "tree.root",
+#                   outputbranchsel="keepOut.txt"
+#                   )
+# p.run()
+
+
 p = PostProcessor(outputDir = ".",
-                  inputFiles = inputFiles(),
+                  inputFiles = ["inFile.root"],
                 #   cut = "",
                   modules=modules,
                   provenance=True,
                   fwkJobReport=True,
-                  jsonInput=runsAndLumis(),
+                  # jsonInput=runsAndLumis(),
                   # maxEntries = 2000,
                   friend = False,
                   haddFileName = "tree.root",
-                  outputbranchsel="keepOut.txt"
+                  outputbranchsel="keepOut.txt",
+                  maxEntries=100000
+                  # maxEntries=1
                   )
 p.run()
 
--- python/postprocessing/examples/variableCalculatorModule.py
+++ python/postprocessing/examples/variableCalculatorModule.py
@@ -18,6 +18,7 @@ from PhysicsTools.variablecalculator.cutflow.CutFlow import CutFlow
 
 class variableCalculatorModule(Module):
     def __init__(self, test=""):
+        print("parsed option in constructor directly: "+test)
         self.var_calc = VariableCalculator()
         self.jet_tag_sel = JetSelection()
         self.lep_sel = LeptonSelection()
@@ -27,11 +28,26 @@ class variableCalculatorModule(Module):
         self.cutflowpath = ""
         self.cutflow = CutFlow()
         self.TestOption =""
+        self.jecs = None
+
         pass
 
-    def setOptions(self, test = ""):
+    def setOptions(self, test = "", jecs = [""]):
         self.testOption = test
-        print("successfully passed test option: " + self.testOption)
+        self.jecs = jecs
+        if self.jecs:
+            # reinit JetSelection
+            self.jet_tag_sel.jecs = self.jecs
+            self.jet_tag_sel.initCounter()
+
+            # reinit variableCalculator
+            print("reinitializing variables")
+            self.var_calc.jecs = self.jecs
+            self.var_calc.output_values = {}
+            self.var_calc.output_arrays = {}
+            self.var_calc.init_values()
+        # print("successfully passed option: " + self.jecs)
+        # exit()
         pass
 
     def beginJob(self, cutflowpath):
@@ -45,22 +61,53 @@ class variableCalculatorModule(Module):
 
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
+        nBranches = 0
         
-        # init scalar branches
-        for key in self.var_calc.output_values:
-            if key == "N_Jets" or key =="N_TightLeptons":
-                self.out.branch(key, "I")
-            else:
-                self.out.branch(key, "F")
+        # pprint(self.var_calc.output_ValueswJEC)
+        # pprint(self.var_calc.output_ArrayswJEC)
+        if self.jecs:
+            for jec in self.jecs:
+                # init scalar branches
+                for key in self.var_calc.output_ValueswJEC[jec]:
+                    # if "N_Jets" in key or "N_TightLeptons" in key:
+                    if "N_" in key:
+                        self.out.branch(key, "I")
+                    else:
+                        self.out.branch(key, "F")
+                    print("init branch {}".format(key))
+                    nBranches +=1
 
-        # init array branches
-        for key in self.var_calc.output_arrays:
-            index_var = "n"+key
-            if "Jet" in key:
-                index_var = "N_Jets"
-            elif "Lepton" in key:
-                index_var = "N_TightLeptons"
-            self.out.branch(key, "F", 1,index_var)
+                try:
+                    # init array branches
+                    outputAr = self.var_calc.output_ArrayswJEC[jec]
+                    for key in outputAr:
+                        index_var = "n"+key
+                        if "Jet" in key:
+                            index_var = "N_Jets_"+jec
+                        elif "Lepton" in key:
+                            index_var = "N_TightLeptons"
+                        # print("index_var : {}".format(index_var))
+                        self.out.branch(key, "F", 1, index_var)
+                        print("init branch {} with index {}".format(key, index_var))
+                        nBranches +=1
+                except:
+                    print("WARNING: No array branches defined in variableCalculator! Make sure this makes sense!")
+        else:
+            for key in self.var_calc.output_values[jec]:
+                    # if "N_Jets" in key or "N_TightLeptons" in key:
+                    if "N_" in key:
+                        self.out.branch(key, "I")
+                    else:
+                        self.out.branch(key, "F")
+            for key in self.var_calc.output_arrays:
+                index_var = "n"+key
+                if "Jet" in key:
+                    index_var = "N_Jets_"+jec
+                elif "Lepton" in key:
+                    index_var = "N_TightLeptons"
+                self.out.branch(key, "F", 1, index_var) 
+
+        print("INFO: Initialized {} branches.".format(nBranches))
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
@@ -68,64 +115,94 @@ class variableCalculatorModule(Module):
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
 
-        self.var_calc.event = event
-
-
         # do selections
         # # global cutflow
         self.cutflow += 1
-        
-        # # Trigger Selection
+
+
+
+        # Trigger Selection
         triggersel = (self.basic_trigger_mc.apply_basic_trigger_mc(event) and self.trigger_filter_mc.apply_trigger_filter_mc(event))
         if not triggersel:
             return False
         self.cutflow.update_cutflow("Basic Trigger MC Selection", self.basic_trigger_mc.counter)
         self.cutflow.update_cutflow("Filter MC Selection", self.trigger_filter_mc.counter)
 
-        # # Lepton Selection
+        # Lepton Selection
         lepsel = self.lep_sel.apply_lepton_selection(event)
         # print("lepsel is {}".format(lepsel))
         if not lepsel:
             return False
         self.cutflow.update_cutflow("Single Lepton Selection", self.lep_sel.counter)
 
-        # # Jet Selection
-        jetsel = self.jet_tag_sel.apply_jet_selection(event)
-        # print("jetsel is {}".format(jetsel))
-        if not jetsel:
-            return False
-        self.cutflow.update_cutflow(">=4 Jet, >=3 Tag Selection", self.jet_tag_sel.counter)
 
         # # MET Selection
         metsel = self.met_sel.apply_met_selection(event)
         if not metsel:
             return False
         self.cutflow.update_cutflow("MET Selection", self.met_sel.counter)
-        
-        # # Final Check Selections
-        is_selected = (jetsel and lepsel and metsel and triggersel)
-        if not is_selected:
+
+        # # Jet Selection
+        # check each JEC individually
+        jetsels = {}
+        for sys in self.jecs:
+            jetsel = self.jet_tag_sel.apply_jet_selection(event, sys = sys)
+            jetsels[sys] = jetsel
+            if jetsel:
+                self.cutflow.update_cutflow(sys+": >=4 Jet, >=3 Tag Selection", self.jet_tag_sel.counter[sys])
+        # check jetselection for all JECs and continue if at least one is True
+        if not any(jetsels.values()):
             return False
+
+
+
+        
+        # # # Final Check Selections
+        # is_selected = (jetsel and lepsel and metsel and triggersel)
+        # if not is_selected:
+        #     return False
 
 
         # weird event instance stuff...
         # if self.var_calc.eventError == True:
             # return False
+
         # calc vars
-        self.var_calc.calculate()
-        
-        # write scalar branches
-        for key in self.var_calc.output_values:
-            self.out.fillBranch(key, self.var_calc.output_values[key])
-        
-        # write array branches
-        # pprint(self.var_calc.output_arrays)
-        for key in self.var_calc.output_arrays:
-            self.out.fillBranch(key, self.var_calc.output_arrays[key])
-        
+        for sys in self.jecs:
+            self.var_calc.resetOutput()
+            # print(sys)
+            # print("#"*20)
+            # print("calculating vars for {}".format(sys))
+            if jetsels[sys]:
+                self.var_calc.sys = sys
+                self.var_calc.event = event
+                self.var_calc.calculate()
+            # else: # reset values in case event gets not selected for a certain jes-> only write dummy vars
+                # self.var_calc.resetOutput()
+                # print(sys)
+                # pprint(self.var_calc.output_ArrayswJEC[sys])
+                # pprint(self.var_calc.resetValuesArray[sys])
+
+            # write scalar branches
+            for key in self.var_calc.output_ValueswJEC[sys]:
+                self.out.fillBranch(key, self.var_calc.output_ValueswJEC[sys][key])
+                # if "N_Jets" in key:
+                    # print("Filling {var} with value {val}".format(var=key,val=self.var_calc.output_ValueswJEC[sys][key]))
+            # Write array branches
+            try:
+                for key in self.var_calc.output_ArrayswJEC[sys]:
+                    self.out.fillBranch(key, self.var_calc.output_ArrayswJEC[sys][key])
+                    # if "Jet_Pt" in key and not jetsels[sys]:
+                        # print("Filling {var} with value {val}".format(var=key,val=self.var_calc.output_ArrayswJEC[sys][key]))
+            except:
+                print("WARNING: No array branches defined, therefore not filling them")
+
+        # print("----------------")
+        # pprint(self.var_calc.output_ValueswJEC)
+        # pprint(self.var_calc.output_ArrayswJEC[sys])
+
         return True
 
 
 # define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
-
-variableCalculatorModuleConstr = lambda: variableCalculatorModule("TEST")
+variableCalculatorModuleConstr = lambda: variableCalculatorModule()
